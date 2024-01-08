53c576cef5ac592a7ac8f185ec4c7786afa568c9
Carl Richard <carl.richard@institutsolacroup.com>
2024-01-08 10:14:44 +0100

    Commit-index.CSS

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// import { Home } from './pages/home';
+// import { AccueilEntreprise } from './Pages/AccueilEntreprise'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/AccueilAdmin" element={<AccueilAdmin />} />
+        <Route path="/AccueilAdminDetails" element={<AccueilAdminDetails />} />
+        <Route path="/AccueilEntreprise" element={<AccueilEntreprise />} />
+        <Route path="/Entreprise/:id" element={<EntrepriseID />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
 
 export default App
+
+
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -10,5 +10,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <NavBar/>
     <AccueilEntreprise/>
+    {/* <FormDetails /> */}
   </React.StrictMode>,
 )
