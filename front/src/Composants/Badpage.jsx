162d7e759a3fcd59e0cf35079027e17bf8dbd5b7
EvanBenyahiaNorves <evan.benyahia-norves@institutsolacroup.com>
2024-01-09 11:04:18 +0100

    Le bas de la page Admin

--- front/src/Composants/Badpage.jsx
+++ front/src/Composants/Badpage.jsx
@@ -1,22 +1,23 @@
 import { NavLink } from 'react-router-dom';
-import Mailto from '../assets/LogoNotimail.png';
-import "../index.css"
 import Envoie from '../assets/envoie.png';
 import Plus from '../assets/plus.png';
+import "../index.css"
 import React, { useState } from 'react';
+import Mailto from '../assets/LogoNotimail.png';
 
-export const UserList = () => {
-  const [UserList, setUserList] = useState('');
+export const Badpage = () => {
+  const [badpage, setBadpage] = useState('');
+ 
 
-  const handleOptionChange = (event) => {
-    setUserList(event.target.value);
-  };
+
   return (
     <>
-    <div>
 
-    </div>
+        <div>
+        <img src={Envoie} alt="" />
+        <img src={Plus} alt="" />
+      </div>
     </>
-  );
 
+  );
 };
