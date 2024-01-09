6f1d774fec7607f305a1ec326a401b8722b6512d
Carl Richard <carl.richard@institutsolacroup.com>
2024-01-09 14:55:07 +0100

    CommitTriEtFetchJson

--- front/src/Pages/AccueilAdmin.jsx
+++ front/src/Pages/AccueilAdmin.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import { CardEntreprise } from "../Composants/CardEntreprise"
+import { useEffect } from "react";
 
 
 export const AccueilAdmin = () => {
+    
+ const [jsonData, setJsonData] = useState([]);
 
+    useEffect(() => {
+      fetch('../json') 
+        .then((res) => res.json())
+        .then((data) => {
+           console.log(data)
+          setJsonData(data);
+        })
+        .catch((err) => console.error(err));
+    }, []);
 
     return (
         <>
