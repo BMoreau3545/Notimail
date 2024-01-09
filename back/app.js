98b5ba39089d1bbb56fff31ef88ca5f591c0f63b
broobroo <dachertmaxime@gmail.com>
2024-01-09 14:12:37 +0100

    ancienne version app.js

--- back/app.js
+++ back/app.js
@@ -1,24 +1,26 @@
 const express = require('express');
-const { Pool } = require('pg');
-require('dotenv').config({
-  path: '../.env',
-});
-
+const sequelize = require('./models/User.js');  // Assurez-vous que le chemin est correct
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 const port = 3000;
 
-const pool = new Pool({
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'postgres',
-  password: '',
-  port: 5432,
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Vérifiez la connexion à la base de données et synchronisez le modèle
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connexion à la base de données établie avec succès.');
+    return sequelize.sync(); // Cela créera les tables s'il n'existe pas
+  })
+  .then(() => {
+    console.log('Tables synchronisées avec succès.');
+  })
+  .catch(err => {
+    console.error('Erreur de connexion à la base de données :', err);
+  });
+
 
 // Montez les routes pour les opérations CRUD liées à l'utilisateur
 app.use('/users', userRoutes);
