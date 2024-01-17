ba38cbc17f368941d9433d099b6135e59227b45e
broobroo <dachertmaxime@gmail.com>
2024-01-17 14:17:34 +0100

    commentaires fichier index.js

--- back/Controller/authController.js
+++ back/Controller/authController.js
@@ -22,7 +22,9 @@ async function login (req, res) {
         // La méthode findOne est utilisée pour trouver un seul enregistrement qui correspond aux critères spécifiés.
 
         // Utilisation de l'opération asynchrone avec le mot-clé 'await' pour attendre que la requête se termine.
-        const user = await db.User.findOne({where: {firm_name: firm_name}}); // Spécification des critères de recherche (utilisation de where). Dans ce cas, on cherche un utilisateur dont la valeur dans la colonne 'firm_name' correspond à la variable 'firm_name'.
+        const user = await db.User.findOne({where: {firm_name: firm_name}}); 
+        // Spécification des critères de recherche (utilisation de where). 
+        //Dans ce cas, on cherche un utilisateur dont la valeur dans la colonne 'firm_name' de la base de données correspond à la variable 'firm_name' récupéré dans la requete.
         console.log(user)
         // Comparaison du mot de passe fourni avec le mot de passe haché stocké dans la base de données
         const passwordMatch = await bcrypt.compare(password, user.password);
@@ -31,12 +33,13 @@ async function login (req, res) {
             // Si les mots de passe correspondent, génération d'un token JWT avec des informations spécifiques
             const token = jwt.sign(
                 { firm_name: user.firm_name },
-                process.env.JWT_SECRET,  // Utilisation d'une clé secrète provenant de variables d'environnement
+                process.env.JWT_SECRET,  // Utilisation d'une clé secrète qui sert à crypter le token provenant de variables d'environnement
                 { expiresIn: process.env.JWT_EXPIRES_IN }  // Spécification de la durée de validité du token (1 heure dans cet exemple)
             );
             // res.json({ token });  // Envoyer le token comme réponse JSON
             // Définir le cookie avec le nom "token"
             res.cookie('token', token, { httpOnly: true, secure: true, expires: new Date(Date.now() + 3600000) });
+            // Réponse indiquant que la connexion est réussie
             res.json({message: 'Connexion réussie', token, user})    
         } else {
             // Si les mots de passe ne correspondent pas, renvoyer une erreur d'authentification
--- back/models/index.js
+++ back/models/index.js
@@ -4,21 +4,26 @@ require('dotenv').config({
 });
 
 // Importation du module Sequelize depuis la bibliothèque sequelize
+// Sequelise est une classe qu'on extrait du module et qu on stocke dans une variable du meme nom
 const { Sequelize } = require('sequelize');
 
 // Importation de la configuration Sequelize depuis le fichier config.js
+// Correspond au module exporté par config
 const config = require('../config/config');
 
 console.log('Configuration Sequelize:', config.development);
 // Création d'une instance Sequelize en utilisant la configuration spécifiée dans config.development
 const sequelize = new Sequelize(config.development);
-
+// on fait la configuration qu on stocke dans l objet db
 const db = {
   Sequelize: Sequelize,
   sequelize: sequelize
 }
-
+// Création de la propriété User dans la configuration db afin de créer la table à l'aide du model User
+// sequelize représente la connexion a la base de donnees
+// Sequalize est la classe Sequelize elle-même
 db.User = require("./User")(sequelize, Sequelize);
 
+//On exporte la configuration sequelize via db afin de le réutiliser 
 module.exports = db;
 
