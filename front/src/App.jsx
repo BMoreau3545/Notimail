72cc9a16b584e2afcd7f1ce96fc48c1fb9b3de30
Carl Richard <carl.richard@institutsolacroup.com>
2024-01-22 10:06:21 +0100

    Ajout fonction delete , et recup info modifier

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,50 +1,60 @@
-// App.js (ou tout autre composant parent)
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from './Pages/LoginPage.jsx';
 import { AccueilEntreprise } from './Pages/AccueilEntreprise.jsx';
 import { AccueilAdmin } from './Pages/AccueilAdmin.jsx';
 import { Error } from './Pages/Error.jsx';
-import { FormDetails } from './Composants/FormDetails.jsx'
+import { FormDetails } from './Composants/FormDetails.jsx';
+import { EditForm } from './Composants/EditForm.jsx';
 
-function App(cardData) {
+function App() {
   const [dataFirmName, setDataFirmName] = useState([]); 
-  //save data fetch
   const [loggedInFirmName, setLoggedInFirmName] = useState('');
-  //enrengistrement du nom de connexion
+  const [loginPassword, setLoginPassword] = useState('');
 
   const updateLoggedInFirmName = (firmName) => {
     setLoggedInFirmName(firmName);
   };
 
-  // const [loginFirm, setLoginFirm] = useState('');
-  const [loginPassword, setLoginPassword] = useState('');
-
-
-  // Fetch base de données 
   useEffect(() => {
     fetch('http://localhost:3000/users/get_all_firm_name')
       .then((res) => res.json())
       .then((dataFirmName) => {
-       setDataFirmName(dataFirmName);
-        console.log(dataFirmName)
+        setDataFirmName(dataFirmName);
+        console.log(dataFirmName);
       })
       .catch((err) => {
         console.error('Erreur de requête fetch :', err);
       });
-      
   }, []);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route
-          path="/" element={<LoginPage loginPassword={loginPassword}  dataFirmName={dataFirmName} updateLoggedInFirmName={updateLoggedInFirmName} loggedInFirmName={loggedInFirmName} />} />
-        <Route path="/admin" element={<AccueilAdmin dataFirmName={dataFirmName} loggedInFirmName={loggedInFirmName} />} />
-        <Route path="/entreprise" element={<AccueilEntreprise dataFirmName={dataFirmName} loggedInFirmName={loggedInFirmName} />} />
-        <Route path="/adminDetails" element={<FormDetails cardData={cardData} dataFirmName={dataFirmName} />} />
-        <Route path="/adminDetails/:firm_name" element={<FormDetails dataFirmName={dataFirmName} />} />
-
+          path="/"
+          element={
+            <LoginPage
+              loginPassword={loginPassword}
+              dataFirmName={dataFirmName}
+              updateLoggedInFirmName={updateLoggedInFirmName}
+              loggedInFirmName={loggedInFirmName}
+            />
+          }
+        />
+        <Route
+          path="/admin"
+          element={<AccueilAdmin dataFirmName={dataFirmName} loggedInFirmName={loggedInFirmName} />}
+        />
+        <Route
+          path="/entreprise"
+          element={<AccueilEntreprise dataFirmName={dataFirmName} loggedInFirmName={loggedInFirmName} />}
+        />
+        <Route
+          path="/adminDetails"
+          element={<FormDetails dataFirmName={dataFirmName} />}
+        />
+        <Route path="/adminDetails/:firm_name" element={<EditForm />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
---
+++ front/src/Composants/EditForm.jsx
@@ -0,0 +1,217 @@
+// Importez les dépendances nécessaires
+import React, { useState, useEffect } from 'react';
+import Mailto from '../assets/LogoNotimail.png';
+import { FaArrowLeftLong } from "react-icons/fa6";
+import '../index.css';
+import { NavLink, useParams } from 'react-router-dom';
+
+
+export const EditForm = ({ loggedInFirmName }) => {
+    const { firm_name: firmNameParam } = useParams();
+    const token = localStorage.getItem('token');
+    const [formData, setFormData] = useState({
+        firm_name: '',
+        first_name: '',
+        last_name: '',
+        phone_number: '',
+        email: '',
+        password: '',
+        isAdmin: false,
+    });
+
+    // Fetch company details on component mount
+    useEffect(() => {
+        fetch(`http://localhost:3000/users/get_user/${firmNameParam}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((companyDetails) => {
+                setFormData({
+                    firm_name: companyDetails.firm_name,
+                    first_name: companyDetails.first_name,
+                    last_name: companyDetails.last_name,
+                    phone_number: companyDetails.phone_number,
+                    email: companyDetails.email,
+                    password: companyDetails.password,
+                    isAdmin: companyDetails.isAdmin,
+                });
+            })
+            .catch((err) => console.error(err));
+    }, [firmNameParam, token]);
+
+    const handleDeleteFirm = () => {
+        const firmName = firmNameParam;
+
+        if (!firmName) {
+            console.error("Nom de l'entreprise manquant");
+            return;
+        }
+
+        fetch(`http://localhost:3000/users/delete_user/${firmName}`, {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    console.error('Erreur lors de la suppression de l\'entreprise');
+                    return;
+                }
+                console.log('Entreprise supprimée avec succès');
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la suppression de l\'entreprise:', error);
+            });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        fetch(`http://localhost:3000/users/update_user/${firmNameParam}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    console.error('Erreur lors de la mise à jour de l\'entreprise');
+                    return;
+                }
+                console.log('Entreprise mise à jour avec succès');
+                // Utilisez la prop history pour la redirection
+                props.history.push(`/company/${firmNameParam}`);
+            })
+            .catch((error) => {
+                console.error('Erreur lors de la mise à jour de l\'entreprise:', error);
+            });
+    };
+
+    // Supprimez la fonction EditEntreprise si elle n'est pas nécessaire
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/users/get_user/${firmNameParam}`, {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((res) => res.json())
+            .then((firmNameParam) => {
+                console.log(firmNameParam);
+            })
+            .catch((err) => console.error(err));
+    }, [firmNameParam]);
+
+    return (
+        <>
+            <section className='SectionForm'>
+                <img src={Mailto} id='NotimailLogo' alt="Logo du site" />
+                <div className='row align'>
+                    <NavLink to="/admin">
+                        <button className='backBtn'>
+                            <FaArrowLeftLong style={{ fontSize: '2rem', color: '#FFFF' }} />
+                        </button>
+                    </NavLink>
+                    <h2 className='NameEntr'>Entreprise</h2>
+                </div>
+                <form className='Formulaire' onSubmit={handleSubmit}>
+                    <div className='row spaceBeetw'>
+                        <label htmlFor="entreprise">Entreprise:</label>
+                        <input
+                            className='inputForm'
+                            type="text"
+                            id="entreprise"
+                            name="firm_name"
+                            value={formData.firm_name}
+                            onChange={(e) => setFormData({ ...formData, firm_name: e.target.value })}
+                        />
+                    </div>
+                    <div className='row spaceBeetw'>
+                        <label htmlFor="contactNom">Contact:</label>
+                        <div className='column'>
+                            <input
+                                className='inputForm'
+                                type="text"
+                                id="contactNom"
+                                name="first_name"
+                                placeholder='Nom'
+                                value={formData.first_name}
+                                onChange={(e) => setFormData({ ...formData, first_name: e.target.value })}
+                            />
+                            <input
+                                className='inputForm'
+                                type="text"
+                                id="contactPrenom"
+                                name="last_name"
+                                placeholder='Prénom'
+                                value={formData.last_name}
+                                onChange={(e) => setFormData({ ...formData, last_name: e.target.value })}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='row spaceBeetw'>
+                        <label htmlFor="tel">Téléphone:</label>
+                        <input
+                            className='inputForm'
+                            type="tel"
+                            id="tel"
+                            name="phone_number"
+                            value={formData.phone_number}
+                            onChange={(e) => setFormData({ ...formData, phone_number: e.target.value })}
+                        />
+                    </div>
+
+                    <div className='row spaceBeetw'>
+                        <label htmlFor="email">Email:</label>
+                        <input
+                            className='inputForm'
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                        />
+                    </div>
+
+                    <div className='row spaceBeetw'>
+                        <label htmlFor="identifiant">Identifiant:</label>
+                        <input
+                            className='inputForm'
+                            type="text"
+                            id="identifiant"
+                            name="password"
+                            value={formData.password}
+                            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                        />
+                    </div>
+
+                    <div className='row'>
+                        <label htmlFor="isAdmin">Admin:</label>
+                        <input
+                            className='inputForm'
+                            type="checkbox"
+                            id="isAdmin"
+                            name="isAdmin"
+                            checked={formData.isAdmin}
+                            onChange={(e) => setFormData({ ...formData, isAdmin: e.target.checked })}
+                        />
+                    </div>
+                    <div className='row'>
+                        <div className='row'>
+                            <button onClick={handleDeleteFirm} type="button" className='deleteBtn'>Supprimer</button>
+                            <button type="submit" className='submitForm'>Modifier</button>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
+};
