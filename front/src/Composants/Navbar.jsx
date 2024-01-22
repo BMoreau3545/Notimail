5a4e39f91eeb6cb9023e0098fef27e102c869e92
Carl Richard <carl.richard@institutsolacroup.com>
2024-01-22 14:27:09 +0100

    navbar logout

--- front/src/Composants/Navbar.jsx
+++ front/src/Composants/Navbar.jsx
@@ -1,76 +1,73 @@
-import { NavLink, useNavigate } from 'react-router-dom';
-import Mailto from '../assets/LogoNotimail.png';
-import { IoMailOpen ,  IoCloseCircle } from "react-icons/io5";
-import { FaCheckCircle } from "react-icons/fa";
-import { Modal } from 'react-responsive-modal';
-import "../Navbar.css";
-import "../index.css"
-import { useState } from 'react';
+  import { useNavigate } from 'react-router-dom';
+  import Mailto from '../assets/LogoNotimail.png';
+  import { IoMailOpen ,  IoCloseCircle } from "react-icons/io5";
+  import { FaCheckCircle } from "react-icons/fa";
+  import { Modal } from 'react-responsive-modal';
+  import "../Navbar.css";
+  import "../index.css"
+  import { useState } from 'react';
 
-export const NavBar = ({ loggedInFirmName, loginPassword, selectedUser}) => {
-  const [open, setOpen] = useState(false);
-  const navigate = useNavigate();
+  export const NavBar = ({ loggedInFirmName, loginPassword, selectedUser}) => {
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
 
-  // Gestion de la modal
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
-  console.log(loggedInFirmName)
+    // Gestion de la modal
+    const onOpenModal = () => setOpen(true);
+    const onCloseModal = () => setOpen(false);
 
-  // Déconnexion de l'utilisateur
-  const handleLogout = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/auth/logout', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          firm_name: loggedInFirmName,
-          password: loginPassword,
-          is_admin: selectedUser === 'admin'
-        }),
-      });
-
-      if (response.ok) {
-        console.log('Utilisateur déconnecté avec succès');
-        // Nettoyez le localStorage
-        // localStorage.removeItem('token');
-        // localStorage.removeItem('firmName');
-        // localStorage.removeItem('isAdmin');
-        localStorage.clear('token');
-        console.log('Après la déconnexion :', localStorage.getItem('token'));
-        // Redirection vers la page de connexion
-        navigate('/');
-
-
-      } else {
-        console.error('Erreur lors de la déconnexion');
+    // Déconnexion de l'utilisateur
+    const handleLogout = async () => {
+      try {
+        console.log('Tentative de déconnexion...');
+    
+        const response = await fetch('http://localhost:3000/auth/logout', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            firm_name: loggedInFirmName,
+            password: loginPassword,
+            is_admin: selectedUser === 'admin'
+          }),
+        });
+    
+        if (response.ok) {
+          console.log('Utilisateur déconnecté avec succès');
+          // Nettoyez le localStorage
+          localStorage.removeItem('token');
+          localStorage.removeItem('firmName');
+          localStorage.removeItem('is_admin');
+          // localStorage.clear('token');
+          console.log('Après la déconnexion :', localStorage.getItem('token'));
+          // Redirection vers la page de connexion
+          navigate('/');
+        } else {
+          console.error('Erreur lors de la déconnexion');
+        }
+      } catch (error) {
+        console.error('Erreur lors de la requête de déconnexion :', error);
       }
-    } catch (error) {
-      console.error('Erreur lors de la requête de déconnexion:', error);
-    }
-  };
+    };
 
-  return (
-    <>
-      <nav className='logo'>
-        <img src={Mailto} id='NotimailLogo' alt="Logo du site" />
-        <div className='center column'>
-          <h3>{loggedInFirmName}</h3>
-          <button onClick={onOpenModal}>Déconnexion</button>
-          <Modal open={open} onClose={onCloseModal} center closeIcon=" ">
-            <p>
-              Confirmer la déconnexion ?
-            </p>
-            <div className='centerIcons'>
-              <IoCloseCircle className='button-react' style={{ fontSize: '60px', color: '#FF3535' }} onClick={onCloseModal} />
-              <NavLink to="/">
-                <FaCheckCircle className='button-react' style={{ fontSize: '50px', color: '#025892' }} onClick={handleLogout} />
-              </NavLink>
-            </div>
-          </Modal>
-        </div>
-      </nav>
-    </>
-  );
-};
+    return (
+      <>
+        <nav className='logo'>
+          <img src={Mailto} id='NotimailLogo' alt="Logo du site" />
+          <div className='center column'>
+            <h3>{loggedInFirmName}</h3>
+            <button onClick={onOpenModal}>Déconnexion</button>
+            <Modal open={open} onClose={onCloseModal} center closeIcon=" ">
+              <p>
+                Confirmer la déconnexion ?
+              </p>
+              <div className='centerIcons'>
+                <IoCloseCircle className='button-react' style={{ fontSize: '60px', color: '#FF3535' }} onClick={onCloseModal} />
+                  <FaCheckCircle className='button-react' style={{ fontSize: '50px', color: '#025892' }} onClick={handleLogout} />
+              </div>
+            </Modal>
+          </div>
+        </nav>
+      </>
+    );
+  };
